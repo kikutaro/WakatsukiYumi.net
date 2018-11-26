cff27b8845922a35e4292e2f6ea0f10e8d1bc1a0
kikutaro <kikutaro526@yahoo.co.jp>
2018-11-26 23:10:16 +0900

    卒業セレモニーまでのカウントダウンを追加

--- js/countdown.js
+++ js/countdown.js
@@ -12,3 +12,18 @@ var x = setInterval(function() {
   document.getElementById("countdown").innerHTML = "若月佑美さんの乃木坂46卒業まで <strong>" + days + "日 " + hours + "時間 "
   + minutes + "分 " + seconds + "秒 " + "</strong>";
 }, 1000);
+
+var countDownDateToCeremony = new Date("2018-12-04T19:00:00").getTime();
+
+var y = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDateToCeremony - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdownCeremony").innerHTML = "若月佑美さんの卒業セレモニーまで <strong>" + days + "日 " + hours + "時間 "
+  + minutes + "分 " + seconds + "秒 " + "</strong>";
+}, 1000);
