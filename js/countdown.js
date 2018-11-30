2c14a4dbd63a0dad90b1e4229e07a6367a9b3346
kikutaro <kikutaro526@yahoo.co.jp>
2018-12-01 00:20:30 +0900

    巻き戻してTweeetボタンを取り消し、卒業からのカウントダウンをカウントアップに修正

--- js/countdown.js
+++ js/countdown.js
@@ -3,7 +3,7 @@ var countDownDate = new Date("2018-11-30T24:00:00").getTime();
 var x = setInterval(function() {
 
   var now = new Date().getTime();
-  var distance = countDownDate - now;
+  var distance = now - countDownDate;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
